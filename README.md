da534f6423496dfc0adb58d6290b7401cfe9fb42
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2014-05-25 20:40:00 +0000

    Update

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ You should create one R script called `run_analysis.R` that does the following :
 -   Uses descriptive activity names to name the activities in the data set
 -   Appropriately labels the data set with descriptive activity names.
 -   Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Run the code
+
+## Dependencies
+
+You will need the \`dplyr\` package \`magrittr\` to run the \`run<sub>analysis.R</sub>\` script
+
+```R
+library(devtools)
+install_github("hadley/dplyr")
+install_github("hadley/magrittr")
+```
--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,17 @@
-library(plyr)
+library(dplyr)
+library(magrittr)
 
 ## Data URL
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
 isdatapres <- file.exists("UCI HAR Dataset")
-
 if (!isdatapres) {
     download.file(url,
                   destfile = "dataset.zip",
                   method = "curl")
-
     unzip("dataset.zip")
     file.remove("dataset.zip")
 }
+
 
 ## Merge training and testing set
 label <- read.table("UCI HAR Dataset/activity_labels.txt",
@@ -34,7 +33,6 @@ y_train <- read.table("UCI HAR Dataset/train/y_train.txt",
                       col.names = "y")
 
 train <- cbind(trainid, x_train, y_train)
-str(train)
 
 ## Test
 testid <- read.table("UCI HAR Dataset/test/subject_test.txt",
@@ -46,40 +44,42 @@ names(x_test) <- colnames
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt",
                      col.names = "y")
 test <- cbind(testid, x_test, y_test)
-str(test)
+
 
 ## Binding training and testing
 data <- rbind(train, test)
-str(data, max.level = 1)
-names(data)
 
-
-## get mean and std
+## Get only mean and std
 meas <- grep("mean\\(|std\\(", names(data), value = TRUE)
 data <- data[, c("subject", "y", meas)]
-str(data)
 
 ## Merge the data and label
-data <- join(data, label)
+data <- inner_join(data, label)
+
+## Remove "y" and reorder columns
 data <- data[, c("subject", "activity", meas)]
-str(data)
 
 cnames <- names(data)[-(1:2)]
 cnames <- gsub("\\-|\\(|\\)", "", cnames) ## remove parenthesis
-cnames <- gsub("^t", "TimeDomain", cnames) ## replace "t" by "TimeDomain"
-cnames <- gsub("^f", "FreqDomain", cnames) ## replace "f" by "FreqDomain"
-cnames <- gsub("Gyro", "Gyroscope", cnames) ## replace "Gyro" by "Gyroscope"
-cnames <- gsub("Acc", "Accelerometer", cnames) ## replace "Acc" by "Accelerometer"
-cnames <- gsub("Mag", "Magnitude", cnames) ## replace "Mag" by "Magnitude"
-cnames <- gsub("BodyBody", "Body", cnames) ## Remove duplicate Body
+cnames <- gsub("^t", "timedomain", cnames) ## replace "t" by "timedomain"
+cnames <- gsub("^f", "freqdomain", cnames) ## replace "f" by "freqdomain"
+cnames <- gsub("Gyro", "gyroscope", cnames) ## replace "Gyro" by "gyroscope"
+cnames <- gsub("Acc", "accelerometer", cnames) ## replace "Acc" by "accelerometer"
+cnames <- gsub("Mag", "magnitude", cnames) ## replace "Mag" by "magnitude"
+cnames <- gsub("BodyBody", "body", cnames) ## Remove duplicate body
 cnames <- gsub("(.+)(std|mean)(X$|Y$|Z$)", "\\1\\3\\2", cnames) ## Put std or mean at the end
-cnames <- gsub("std", "Std", cnames) ## Replace std by Std
-cnames <- gsub("mean", "Mean", cnames) ## Replace mean by Mean
+cnames <- tolower(cnames)
 names(data)[-(1:2)] <- cnames
 
 ## Create the final aggregated data sets
-data_agg <- ddply(data, .(subject, activity), numcolwise(mean))
-str(data_agg)
+data_agg <- data %>%
+    group_by(subject, activity) %>%
+        summarise_each(funs(mean))
 
-## Save tidy datasets
+## Save the tidy datasets
 write.table(data_agg, "tidydata.txt", row.names = FALSE)
+
+
+
+
+
